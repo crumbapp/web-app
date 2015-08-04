2ec24f0da8f830d9dda8b69e8863be4471fdf939
Jimmy Gong <gong.jim@gmail.com>
2015-08-04 11:36:49 -0700

    Sets up nginx to serve static files

--- api/server/index.js
+++ api/server/index.js
@@ -1,8 +1,15 @@
 import express from 'express';
+import morgan from 'morgan';
+import path from 'path';
 
 /* * Create Express Server * */
 const app = express();
 
+app.use(morgan('dev'));
+// app.use(express.static(path.join(__dirname, '../../public/dist')));
+app.get('/hello', (req, res) => {
+  res.sendStatus(200);
+});
 app.set('port', process.env.PORT || 5000);
 app.server = app.listen(app.get('port'), () => {
   console.log('Listening on port %d', app.get('port'));
